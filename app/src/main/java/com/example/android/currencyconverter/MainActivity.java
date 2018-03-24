6144b21ec7e9603aa84afd19d3c5d5f41b9e326e
rahul <krishrahul98@gmail.com>
2018-03-24 13:26:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/currencyconverter/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void dollarToInr(View view){
+        EditText convert = findViewById(R.id.dollar);
+        if (convert.getText().toString().trim().equalsIgnoreCase("")) {
+            convert.setError("This field can not be blank");
+        }
+        else {
+        Double dollarAmount = Double.parseDouble(convert.getText().toString());
+        Double inr=dollarAmount*64;
+        Toast.makeText(MainActivity.this,"Rs "+inr,Toast.LENGTH_LONG).show();
+    }}
+}
